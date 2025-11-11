d56cd14cb44f17808506f055e0f459a6485f2bc8
Vimarsh Sharma <vimarshsharmabsr2000@gmail.com>
2025-11-11 17:48:58 +0530

    Handled exception of creating another entry with same email

--- patient-service/src/main/java/com/vimarsh/patient_service/service/PatientService.java
+++ patient-service/src/main/java/com/vimarsh/patient_service/service/PatientService.java
@@ -2,6 +2,7 @@ package com.vimarsh.patient_service.service;
 
 import com.vimarsh.patient_service.dto.PatientRequestDTO;
 import com.vimarsh.patient_service.dto.PatientResponseDTO;
+import com.vimarsh.patient_service.exception.EmailAlreadyExistsException;
 import com.vimarsh.patient_service.model.Patient;
 import com.vimarsh.patient_service.repository.PatientRepository;
 import com.vimarsh.patient_service.mapper.PatientMapper;
@@ -24,6 +25,9 @@ public class PatientService {
     }
 
     public PatientResponseDTO CreatePatient(PatientRequestDTO patientRequestDTO){
+        if(patientRepository.existsByEmail(patientRequestDTO.getEmail())){
+            throw new EmailAlreadyExistsException(patientRequestDTO.getEmail()) ;
+        }
         Patient patient = PatientMapper.toModel(patientRequestDTO) ;
         patientRepository.save(patient);
         return PatientMapper.toDTO(patient) ;
