3bbbb6b68feaed398e2ca0651d0c353bf49066e1
Vimarsh Sharma <vimarshsharmabsr2000@gmail.com>
2025-12-18 15:35:08 +0530

    Completed Authentication with checks
    - exception handler added
    - changed buggy route for validation(token)

--- api-gateway/src/main/java/com/vimarsh/api_gateway/filter/JwtValidationGatewayFilterFactory.java
+++ api-gateway/src/main/java/com/vimarsh/api_gateway/filter/JwtValidationGatewayFilterFactory.java
@@ -1,13 +1,21 @@
 package com.vimarsh.api_gateway.filter;
 
 import io.netty.handler.codec.http.websocketx.extensions.WebSocketClientExtensionHandler;
+import jakarta.validation.constraints.Null;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.gateway.filter.GatewayFilter;
 import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.client.WebClient;
 
-public class JwtValidationGatewayFilterFactory extends AbstractGatewayFilterFactory {
+@Component
+public class JwtValidationGatewayFilterFactory extends AbstractGatewayFilterFactory<Object> {
 
+    private static final Logger log = LoggerFactory.getLogger(JwtValidationGatewayFilterFactory.class);
     private final WebClient webClient ;
 
     public JwtValidationGatewayFilterFactory(WebClient.Builder builder,
@@ -18,6 +26,20 @@ public class JwtValidationGatewayFilterFactory extends AbstractGatewayFilterFact
 
     @Override
     public GatewayFilter apply(Object config) {
-        return null;
+        return (exchange, chain) -> {
+          String token= exchange.getRequest().getHeaders()
+                  .getFirst(HttpHeaders.AUTHORIZATION) ;
+          if(token == null || !token.startsWith("Bearer ")){
+              exchange.getResponse().setStatusCode(HttpStatus.UNAUTHORIZED) ;
+              return exchange.getResponse().setComplete() ;
+          }
+          log.info("Token-> "+token) ;
+          return webClient.get()
+                  .uri("/auth/validate")
+                  .header(HttpHeaders.AUTHORIZATION, token)
+                  .retrieve()
+                  .toBodilessEntity()
+                  .then(chain.filter(exchange)) ;
+        };
     }
 }
