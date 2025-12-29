a3001811252bb731641d954db77c2a14aa723086
Vimarsh Sharma <vimarshsharmabsr2000@gmail.com>
2025-12-29 21:50:24 +0530

    labTest Service module added and initialized

---
+++ labtestService/src/main/java/com/vimarsh/labtestService/LabtestServiceApplication.java
@@ -0,0 +1,13 @@
+package com.vimarsh.labtestService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LabtestServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LabtestServiceApplication.class, args);
+	}
+
+}
---
+++ labtestService/src/test/java/com/vimarsh/labtestService/LabtestServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vimarsh.labtestService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LabtestServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
